a155743f8492be3559fa8220594d09d39903b51f
amasirat <amir.basirat.2004@gmail.com>
2025-02-04 18:08:34 +0330

    made seperate component for displaying comments

---
+++ resources/views/components/blog/recursive_comment.blade.php
@@ -0,0 +1,11 @@
+@props("comments")
+@php
+function recursive_comment_display($comment, $level)
+{
+
+}
+@endphp
+
+@foreach($comments as $comment)
+
+@endforeach
--- resources/views/user/comments.blade.php
+++ resources/views/user/comments.blade.php
@@ -1,8 +1,15 @@
 <x-layout>
     <div>
-        <div class="space-y-10">
+        <div class="space-y-14 mt-10">
             @foreach($comments as $comment)
-                <x-blog.comment :comment="$comment" />
+            <div>
+                <a href="/blogs/{{$comment->blog->id}}" class="text-white">
+                <div class="p-5 bg-white/15 w-2/5 max-md:w-full hover:w-3/4 hover:bg-white/30 duration-300" >
+                    <span class="text-white/15">from "{{ $comment->blog->title }}"</span>
+
+                    <x-blog.comment :comment="$comment" />
+                </div></a>
+            </div>
             @endforeach
         </div>
     </div>
