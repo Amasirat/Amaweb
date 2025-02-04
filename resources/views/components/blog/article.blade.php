1c691e78997f826448623a03d9c9221360a9dfcf
amasirat <amir.basirat.2004@gmail.com>
2025-02-04 15:01:03 +0330

    fixed article title overflow problem

--- resources/views/components/blog/article.blade.php
+++ resources/views/components/blog/article.blade.php
@@ -1,10 +1,13 @@
 @props(["blog"])
 
 @php
+
     if(! is_null($blog->image))
         $blogImage = asset('storage/'.$blog->image);
     else
         $blogImage = "https://placehold.co/500x400";
+
+    $title_limit = 33; // The minimum length that can be outputed without overflow
 @endphp
 <a class="text-white" href="/blogs/{{ $blog->id }}">
     <div class="bg-white/5 hover:bg-white/25 duration-300 w-96 h-96 rounded-xl p-2 shadow-2xl overflow-hidden">
@@ -12,8 +15,12 @@
             <img class="shadow-2xl rounded-md w-96 h-72" src="{{ $blogImage }}" alt="blog-image">
         </div>
 
-        <div class="p-5">
-            <span class="font-bold text-xl">{{ $blog->title }}</span>
+        <div class="p-2">
+            <span class="font-bold text-xl">
+                {{ (strlen($blog->title) < $title_limit) ?
+                    $blog->title :
+                    substr($blog->title, 0, $title_limit)."..."}}
+            </span>
         </div>
     </div>
 </a>
