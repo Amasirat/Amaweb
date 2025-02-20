f2f9955fb932f2d90957de5f6316a8b700cf878f
amasirat <amir.basirat.2004@gmail.com>
2025-02-20 16:48:46 +0330

    refined comment.js's code

--- resources/js/comment.js
+++ resources/js/comment.js
@@ -2,23 +2,24 @@ let hide = "hidden";
 document.addEventListener("DOMContentLoaded", function () {
     // Handle Edit Buttons
     document.querySelectorAll('.edit-comment').forEach(editButton => {
-        editButton.addEventListener('click', () => {
-            const commentWrapper = editButton.closest('.comment-wrapper');
-            const textBox = commentWrapper.querySelector('.comment-text');
-            const commentBox = commentWrapper.querySelector('.edit-comment-box');
+        const commentWrapper = editButton.closest('.comment-wrapper');
+        const textBox = commentWrapper.querySelector('.comment-text');
+        const commentBox = commentWrapper.querySelector('.edit-comment-box');
+        const replyButton = commentWrapper.querySelector('.reply-button');
+        const cancelButton = commentWrapper.querySelector('.cancel-edit');
 
-            commentBox.classList.remove('hidden');
-            editButton.classList.add('hidden');
-            textBox.classList.add('hidden');
+        editButton.addEventListener('click', () => {
+            commentBox.classList.remove(hide);
+            editButton.classList.add(hide);
+            textBox.classList.add(hide);
+            replyButton.classList.add(hide);
         });
-    });
     // Handling canceling editing
-    document.querySelectorAll('.cancel-edit').forEach(cancelButton => {
         cancelButton.addEventListener("click", () => {
-            const commentWrapper = cancelButton.closest('.comment-wrapper');
-            commentWrapper.querySelector(".edit-comment").classList.remove(hide);
-            commentWrapper.querySelector(".comment-text").classList.remove(hide);
-            commentWrapper.querySelector(".edit-comment-box").classList.add(hide);
+            commentBox.classList.add(hide);
+            editButton.classList.remove(hide);
+            textBox.classList.remove(hide);
+            replyButton.classList.remove(hide);
         });
     });
 
