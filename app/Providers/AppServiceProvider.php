f39ffcb3371a9d77e1c26a7cbbf8fe2ddd7801a2
amasirat <amir.basirat.2004@gmail.com>
2025-03-09 15:48:52 +0330

    changed public_html back to public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->usePublicPath(__DIR__.'/../../public_html');
     }
 
     /**
