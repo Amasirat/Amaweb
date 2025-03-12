621c02c8009daad7bebe92819f2299e97627755e
amasirat <amir.basirat.2004@gmail.com>
2025-03-13 01:00:26 +0330

    scheduled weekly draft deletion

---
+++ app/Jobs/DeleteInactiveDrafts.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use App\Models\Draft;
+
+class DeleteInactiveDrafts implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $inactive_drafts = Draft::where("active", false);
+
+        foreach($inactive_drafts as $draft)
+        {
+            Draft::delete_draft($draft);
+        }
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Jobs\DeleteInactiveDrafts;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::call(function () {
+    DeleteInactiveDrafts::dispatch();
+})->weekly()->mondays()->at('1:00');
